bd187d4f265c86638beda92a1e138da827fd586b
shorenashorena <shorenabad@gmail.com>
2025-11-20 22:09:08 +0400

    ფუნქცია 1,2

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function isEmptyString(str = "") {
+  if (typeof str !== "string") {
+    return false;
+  }
+  return str.length === 0;
+}
+
+console.log(isEmptyString("")); 
+console.log(isEmptyString("world"));  
+
+function convertToGel(priceUsd = 0, rate = 0) {
+  if (!priceUsd || !rate) {
+    return 0;
+  }
+  return priceUsd * rate;
+}
+
+console.log(convertToGel(10, 3.5));
+console.log(convertToGel(100, 3.5));
+
+ 
