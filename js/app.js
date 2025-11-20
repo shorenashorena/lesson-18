380cd29341c5d216d4ab313a08fdd6aed9ffe817
shorenashorena <shorenabad@gmail.com>
2025-11-20 23:54:52 +0400

    function getCountryByCurrency

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@ function isEmptyString(str = "") {
   return str.length === 0;
 }
 
-console.log(isEmptyString("")); 
-console.log(isEmptyString("world"));  
+console.log(isEmptyString(""));
+console.log(isEmptyString("world"));
+
 
 function convertToGel(priceUsd = 0, rate = 0) {
   if (!priceUsd || !rate) {
@@ -18,4 +19,20 @@ function convertToGel(priceUsd = 0, rate = 0) {
 console.log(convertToGel(10, 3.5));
 console.log(convertToGel(100, 3.5));
 
- 
+function getCountryByCurrency(currency = "") {
+  switch (currency) {
+    case "USD":
+      return "United States";
+    case "EUR":
+      return "European Union";
+    case "GEL":
+      return "Georgia";
+    default:
+      return "Unknown currency";
+  }
+}
+
+console.log(getCountryByCurrency("USD"));
+console.log(getCountryByCurrency("EUR"));
+console.log(getCountryByCurrency("GEL"));
+console.log(getCountryByCurrency("JPY")); 
